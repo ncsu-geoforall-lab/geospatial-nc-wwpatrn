2750f1155056f18d084d9d842a50f947fd3bfa9f
Vaclav Petras <wenzeslaus@gmail.com>
2020-10-07 14:51:07 -0400

    Add contours and buffer

--- run.py
+++ run.py
@@ -39,14 +39,22 @@ def main():
     cost = options["output"]
     isochrones = "isochrones"  # options["output"]
 
+    raster_network = "raster_network"
+    network_buffer = "network_buffer"
+    masked_cost = "masked_cost"
+
+    resolution = 10
+
     # generate simplified isochrones using cost surface
-    gs.run_command("g.region", res=20)
+    # By setting a region here, we behave more like a script then a module.
+    gs.run_command("g.region", vector=network, res=resolution)
+
     # convert sewer network to raster using velocity - here just constant=1
-    gs.run_command("v.to.rast", input=network, output="sewer", use="val", val=1)
-    # convert to cost (time per cell) and fill in nulls - not necessary r.cost has null_cost option
-    # and here I just kept 1, but it needs to be cel_length/velocity
+    gs.run_command("v.to.rast", input=network, output=raster_network, use="val", val=1)
+    # convert to cost (time per cell)
+    # cell_length/velocity
     gs.mapcalc(
-        "sewer_cost = if(isnull(sewer), 10, 1)"
+        f"sewer_cost = {resolution} / {raster_network}"
     )  # (not necessary r.cost has null_cost option
     # ten we run cumulative cost with wwtp as start point - I used point at the end of the pipe, not the station, which is off
     gs.run_command(
@@ -55,13 +63,22 @@ def main():
         input="sewer_cost",
         output=cost,
         outdir="sewer_se_20ft_costdir",
+        null_cost=20,
         start_coordinates=(2102621.7, 706047.4),
     )
+    # create 3hr contours
+    max_time = 10800
     gs.run_command(
-        "r.contour", input=cost, output=isochrones, step=400, min=0, max=3000
+        "r.contour", input=cost, output=isochrones, step=400, min=0, max=max_time
     )
-    # attempt at
-    # r.watershed elev=sewer_se_20ft_costc basin=sewer_basin1 thresh=10000 accum=sewer_accum -s
+
+    gs.run_command(
+        "r.buffer", input=raster_network, output=network_buffer, distance=300
+    )
+    gs.mapcalc(
+        f"{masked_cost} = if({network_buffer}, int({cost} / {max_time}), null())"
+    )
+    gs.run_command("r.colors", map=masked_cost, color="roygbiv")
 
 
 if __name__ == "__main__":
