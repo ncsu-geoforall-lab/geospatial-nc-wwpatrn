5edb2b1b5e0c9d7e3b50af102fb9bb81e9333e54
Vaclav Petras <wenzeslaus@gmail.com>
2020-10-07 15:41:15 -0400

    Coords and col as inputs, region as implicit input, better time handling

--- run.py
+++ run.py
@@ -14,16 +14,23 @@ This executable script is a GRASS GIS module to run in a GRASS GIS session.
 """
 
 #%module
-#% description: Adds the values of two rasters (A + B)
+#% description: Computes isochrones from collection point in a sewershed
 #% keyword: raster
-#% keyword: algebra
-#% keyword: sum
+#% keyword: cost surface
+#% keyword: time
+#% keyword: network
 #%end
-##%option G_OPT_R_INPUT
+##%option G_OPT_R_ELEV
 ##%end
 #%option G_OPT_V_INPUT
+#% label: Sewer network
+#%end
+#%option G_OPT_DB_COLUMN
+#% label: Velocity column
 #%end
 #%option G_OPT_R_OUTPUT
+#%end
+#%option G_OPT_M_COORDS
 #%end
 
 
@@ -36,47 +43,56 @@ def main():
     """Parse command line and run processing"""
     options, unused_flags = gs.parser()
     network = options["input"]
+    velocity_column = options["column"]
     cost = options["output"]
     isochrones = "isochrones"  # options["output"]
 
     raster_network = "raster_network"
     network_buffer = "network_buffer"
     masked_cost = "masked_cost"
+    base_cost = "base_cost"
 
-    resolution = 10
+    coordinates = options["coordinates"]
 
     # generate simplified isochrones using cost surface
-    # By setting a region here, we behave more like a script then a module.
-    gs.run_command("g.region", vector=network, res=resolution)
+
+    current_region = gs.region()
+    resolution = (current_region["nsres"] + current_region["ewres"]) / 2
 
     # convert sewer network to raster using velocity - here just constant=1
-    gs.run_command("v.to.rast", input=network, output=raster_network, use="val", val=1)
+    gs.run_command(
+        "v.to.rast",
+        input=network,
+        output=raster_network,
+        use="attr",
+        attribute_column=velocity_column,
+    )
     # convert to cost (time per cell)
     # cell_length/velocity
-    gs.mapcalc(
-        f"sewer_cost = {resolution} / {raster_network}"
-    )  # (not necessary r.cost has null_cost option
+    gs.mapcalc(f"{base_cost} = {resolution} / {raster_network}")
     # ten we run cumulative cost with wwtp as start point - I used point at the end of the pipe, not the station, which is off
     gs.run_command(
         "r.cost",
         flags="k",
-        input="sewer_cost",
+        input=base_cost,
         output=cost,
-        outdir="sewer_se_20ft_costdir",
         null_cost=20,
-        start_coordinates=(2102621.7, 706047.4),
+        start_coordinates=coordinates,
     )
     # create 3hr contours
-    max_time = 10800
+    max_time_h = 12
+    max_time_s = max_time_h * 60 * 60
+    n_steps = 20
+    step = max_time_s / n_steps
     gs.run_command(
-        "r.contour", input=cost, output=isochrones, step=400, min=0, max=max_time
+        "r.contour", input=cost, output=isochrones, step=step, min=0, max=max_time_s
     )
 
     gs.run_command(
         "r.buffer", input=raster_network, output=network_buffer, distance=300
     )
     gs.mapcalc(
-        f"{masked_cost} = if({network_buffer}, int({cost} / {max_time}), null())"
+        f"{masked_cost} = if({network_buffer}, round({cost} / {max_time_s}, 0.5), null())"
     )
     gs.run_command("r.colors", map=masked_cost, color="roygbiv")
 
