62f6963228f525911938f6dcc661f6bca4213977
Vaclav Petras <wenzeslaus@gmail.com>
2020-10-09 17:05:42 -0400

    Parametrize the flow inputs computation

--- compute_flow_columns.py
+++ compute_flow_columns.py
@@ -6,6 +6,31 @@ Note that this computation is only partial.
 Slope is computed in the spatial part.
 """
 
+#%module
+#% description: Computes isochrones from collection point in a sewershed
+#% keyword: raster
+#% keyword: cost surface
+#% keyword: time
+#% keyword: network
+#%end
+#%option G_OPT_V_INPUT
+#% label: Sewer network
+#% description: Needs to be in the current mapset (columns will be added)
+#%end
+#%option G_OPT_DB_COLUMN
+#% key: diameter_column
+#% label: Diameter column
+#% required: yes
+#%end
+#%option
+#% key: liquid_height_coefficient
+#% type: string
+#% label: Coefficient for height of liquid in pipe
+#% answer: 0.25
+#% required: yes
+#%end
+
+
 import sys
 
 import grass.script as gs
@@ -25,8 +50,10 @@ def add_float_column_py(vector, name, expression):
 
 def main():
     """Process command line parameters and run computation"""
-    vector = sys.argv[1]
-    diameter = "DIAMETER"
+    options, unused_flags = gs.parser()
+    vector = options["input"]
+    diameter = options["diameter_column"]
+    liquid_height_coefficient = float(options["liquid_height_coefficient"])
 
     gs.run_command(
         "v.db.update", map=vector, column=diameter, value=1, where=f"{diameter} == 0"
@@ -34,9 +61,11 @@ def main():
 
     add_float_column_sql(vector, "diameter_ft", f"{diameter} / 12")  # inch to feet
     add_float_column_sql(vector, "radius_ft", "diameter_ft / 2")  # r = d / 2
-    add_float_column_sql(vector, "diameter_fourth_ft", "diameter_ft / 4")
+    add_float_column_sql(
+        vector, "liquid_height", f"{liquid_height_coefficient} * diameter_ft"
+    )
     add_float_column_py(
-        vector, "theta", "2 * math.acos((radius_ft - diameter_fourth_ft) / radius_ft)"
+        vector, "theta", "2 * math.acos((radius_ft - liquid_height) / radius_ft)"
     )
     add_float_column_py(
         vector, "cs_area", "radius_ft * radius_ft * (theta - math.sin(theta)) / 2"
