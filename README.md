677384d023b529d80309b608fdc8ae9973be7753
Vaclav Petras <wenzeslaus@gmail.com>
2020-12-02 16:08:02 -0500

    Workflow for updated visualization

--- README.md
+++ README.md
@@ -2,15 +2,34 @@
 
 ## Initial setup
 
-### Software
+### Main software
 
-Install GRASS GIS 7.8. Install v.db.pyupdate module, e.g., from command line:
+Install GRASS GIS 7.8.
+
+### Extensions
+
+Install v.db.pyupdate module, e.g., from command line:
 
 ```
 g.extension v.db.pyupdate
 ```
 
+v.out.keplergl module is not in addons yet, so you need to take the script manually
+from a GitHub repo and specify path to it when you want to run it.
+The script is in this repo: https://github.com/ncsu-geoforall-lab/v.out.keplergl/
+
 ## Run
+
+### Flow within a sewershed
+
+See the file `run.sh` for individual steps to do in GRASS GIS.
+The file shows that most steps can be done running modules or scripts in command line.
+
+The scripts behave like GRASS GIS modules in the sense that in command line
+you can add `--help` and get basic usage instructions
+(you can also view the code for complete, but less readable version of that).
+Adding `--ui` in the command line makes GUI for the script appear
+which might be good especially for the first-time exploration of the script.
 
 ### Convert daily COVID data to one table
 
--- run.sh
+++ run.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
-# This script is not meant for running yet.
+# This script is not meant for running automatically,
+# but it provides information on which steps need to be done.
 
 r.in.usgs -k product=ned output_name=elevation output_directory=~/Downloads/r_in_usgs ned_dataset=ned13sec memory=16000 nprocs=4
 
@@ -9,9 +10,16 @@ v.db.join map=lines column=MATERIAL other_table=pipe_roughness_table other_colum
 
 v.db.update map=lines column=roughness query_column="cast(n as DOUBLE)"
 
-./geospatial-nc-wwpatrn/run.py input=lines elevation=elevation roughness_column=n hydraulic_radius_column=hydraulic_radius output=cost coordinates=2151499.4,717482.6 --o
-
 v.db.pyupdate map=lines column=facility_id_int expression="int(FACILITYID[4:])" where="FACILITYID IS NOT NULL AND FACILITYID != ''"
 v.db.pyupdate map=lines column=facility_id_int expression="1" where="FACILITYID IS NULL OR FACILITYID == ''"
 
-./geospatial-nc-wwpatrn/compute_flow_columns.py lines diameter_column=DIAMETER liquid_height_coefficient="0.25"
+# Run scripts using the path to them.
+# In command line, possibly add Python executable, esp. on Windows.
+./compute_flow_columns.py lines diameter_column=DIAMETER liquid_height_coefficient="0.25"
+./compute_cost.py input=lines elevation=elevation roughness_column=n hydraulic_radius_column=hydraulic_radius output=cost coordinates=2151499.4,717482.6
+./create_visualization_helpers.py cost=cost network_buffer=network_buffer raster_choropleth=raster_choropleth vector_choropleth=vector_choropleth max_time=5 time_step=0.5
+
+# Now copy vector_choropleth to WGS84 location and switch there.
+
+# You need to manually specify the path to the script.
+../v.out.keplergl/v.out.keplergl.py input=vector_choropleth label="Flow time" output=sewershed_times.html title="Sewershed times for Raleigh" --o color_column=time style=sewershed_style.dict zoom=10 columns=flow
